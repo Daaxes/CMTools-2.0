da37833447c3e92ae4b1a8780bc78f2cef1f6958
Dan Axelsson <daaxes@gmail.com>
2024-07-09 23:58:15 +0200

    Add project files.

---
+++ CMTools 2.0/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CMTools_2_0.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMTools_2_0.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Organisation> Organisations { get; set; }
+
+    }
+}
---
+++ CMTools 2.0/Data/Migrations/20240709215632_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMTools_2_0.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organisations",
+                columns: table => new
+                {
+                    OrganisationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrganisationName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organisations", x => x.OrganisationId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Organisations");
+        }
+    }
+}
